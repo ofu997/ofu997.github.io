af0c302f905620577f75363bd1e54e6cc9fd3c0d
ofu997 <ofu997@gmail.com>
2020-04-16 16:30:52 -0700

    format page from stringified json to regular things

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
-// import Project from '../components/project';
+import Project from '../components/project';
 
 export const query = graphql`
   query($slug: String!) {
@@ -22,9 +22,20 @@ export const query = graphql`
 
 const ProjectTemplate = ({ data }) => {
   const project = data.projectsJson; 
+  const title = project.title;
+  const description = project.description;
+  const url = project.url;
+  const imageData = project.image.childImageSharp.fluid; 
+
   return (
     <Layout>
-      <pre>{ JSON.stringify(project, null, 2) }</pre>
+      <Project 
+        title={ title }
+        description={ description }
+        url={ url }
+        imageData={ imageData }
+      />
+      {/* <pre>{ JSON.stringify(project, null, 2) }</pre> */}
     </Layout>
   )
 }
