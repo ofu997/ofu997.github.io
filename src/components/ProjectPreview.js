4264498fdd3ae2f4a921b17ee092bf28edb05c71
oliver fu <ofu997@gmail.com>
2021-08-12 18:36:47 -0700

    1) add project 2) change image padding 3) rearrange components in project preview and project page

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -4,18 +4,18 @@ import React from 'react';
 
 const ProjectPreview = ({ slug, title, shortDescription, imageData }) => 
   <div className='project-preview project-container'>
+    <h2>
+      <Link to={ `/${ slug }/` }>{ title }</Link>
+    </h2>
     <Link to={ `/${ slug }/` }>
       <Image fluid={ imageData } alt={ title } />
     </Link>
-      <h2>
-        <Link to={ `/${ slug }/` }>{ title }</Link>
-      </h2>
-      <p>
-        { shortDescription }
-      </p>
-      <p>
-        <Link to={ `/${ slug }/` }>View this project</Link>
-      </p>
+    <p id='shortDescription'>
+      { shortDescription }
+    </p>
+    <p>
+      <Link to={ `/${ slug }/` }>View this project</Link>
+    </p>
   </div>
 
 export default ProjectPreview;
--- src/components/project.js
+++ src/components/project.js
@@ -7,21 +7,19 @@ const Project = ({ title, description, url, secondImageData }) => {
 
     <h2 style={{ margin: '10% 0% 2% 10%' }}>{ title }</h2>
 
-    <div  className='projectPageImageContainer'
-      // style={{ padding: '0% 35%' }}
-    >
-      <Image fluid={ secondImageData } alt={ title } style={{ maxHeight: '25%' }} />
-    </div>      
-
     <div style={{ margin: '5%' }}>
-      <p>{ description }</p>
       <p>
         <a href={ url } target='_blank' rel='noopener noreferrer'>View this project online &rarr;</a>
       </p>
       <p>
         <Link to='/'>&larr; Home</Link>
       </p>
+      <p>{ description }</p>
     </div>
+
+    <div  className='projectPageImageContainer'>
+      <Image fluid={ secondImageData } alt={ title } style={{ maxHeight: '25%' }} />
+    </div>      
   </div>
   );
 };
